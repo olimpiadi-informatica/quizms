284b845b6575f1092e2d8c0ea9728b5561a983ec
Alessandro Bortolin <bortolin.alessandro@outlook.it>
2023-07-12 23:30:36 +0200

    Improve blockly interface

--- src/ui/blockly/blockly.tsx
+++ src/ui/blockly/blockly.tsx
@@ -5,6 +5,7 @@ import { Workspace } from "blockly/core/workspace";
 import { javascriptGenerator } from "blockly/javascript";
 import locale from "blockly/msg/it";
 import { BlocklyWorkspace } from "react-blockly";
+import { ArrowRight, FastForward, Pause, Play, RotateCcw, SkipForward } from "react-feather";
 
 import toolboxConfiguration from "./blocklyToolbox.json";
 import "./customBlocks";
@@ -56,15 +57,60 @@ export function Blockly({ initialBlocks, debug }: BlocklyProps) {
   );
 
   return (
-    <div className="mb-4 overflow-hidden rounded-xl border-2 border-[#c6c6c6]">
-      <BlocklyWorkspace
-        toolboxConfiguration={toolboxConfiguration}
-        workspaceConfiguration={workspaceConfiguration}
-        onWorkspaceChange={onWorkspaceChange}
-        initialJson={json}
-        onJsonChange={onJsonChange}
-        className="h-[30rem] border-0 text-gray-700"
-      />
+    <div className="flex gap-3">
+      <div className="grow">
+        <div className="mb-5 overflow-hidden rounded-xl border-2 border-[#c6c6c6]">
+          <BlocklyWorkspace
+            toolboxConfiguration={toolboxConfiguration}
+            workspaceConfiguration={workspaceConfiguration}
+            onWorkspaceChange={onWorkspaceChange}
+            initialJson={json}
+            onJsonChange={onJsonChange}
+            className="h-[30rem] border-0 text-gray-700"
+          />
+        </div>
+        <div className="mb-5 flex">
+          <textarea
+            rows={4}
+            className="textarea-bordered textarea w-full resize-none font-mono placeholder:font-sans"
+            placeholder="Input"
+          />
+          <div className="divider divider-horizontal">
+            <ArrowRight className="h-20 w-20" />
+          </div>
+          <textarea
+            rows={4}
+            className="textarea-bordered textarea w-full resize-none font-mono placeholder:font-sans"
+            placeholder="Output"
+          />
+        </div>
+      </div>
+      <div className="flex flex-col gap-3">
+        <div className="tooltip" data-tip="Esegui/ferma">
+          <div className="btn h-full w-full p-0">
+            <label className="swap swap-rotate h-full w-full">
+              <input type="checkbox" />
+              <Pause className="swap-on h-6 w-6" />
+              <Play className="swap-off h-6 w-6" />
+            </label>
+          </div>
+        </div>
+        <div className="tooltip" data-tip="Esegui un blocco">
+          <button className="btn">
+            <SkipForward className="h-6 w-6" />
+          </button>
+        </div>
+        <div className="tooltip" data-tip="Esegui fino alla fine">
+          <button className="btn">
+            <FastForward className="h-6 w-6" />
+          </button>
+        </div>
+        <div className="tooltip" data-tip="Esegui da capo">
+          <button className="btn">
+            <RotateCcw className="h-6 w-6" />
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
