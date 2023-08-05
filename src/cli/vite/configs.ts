945e049a30982244e1d9503eeb81d9e0527b30f7
Alessandro Bortolin <bortolin.alessandro@outlook.it>
2023-08-05 13:21:08 +0200

    Remove unused dependencies

--- src/cli/vite/configs.ts
+++ src/cli/vite/configs.ts
@@ -1,5 +1,4 @@
 import mdx from "@mdx-js/rollup";
-import replace from "@rollup/plugin-replace";
 import url from "@rollup/plugin-url";
 import react from "@vitejs/plugin-react-swc";
 import { InlineConfig, splitVendorChunkPlugin } from "vite";
@@ -34,12 +33,6 @@ export default function (mode: "development" | "production"): InlineConfig {
       asymptote(),
       { enforce: "pre", ...mdx(mdxOptions) },
       react({ plugins: swcPlugins }),
-      replace({
-        preventAssignment: true,
-        values: {
-          "process.env.NODE_ENV": `"${mode}"`,
-        },
-      }),
       splitVendorChunkPlugin(),
       url(),
     ],
