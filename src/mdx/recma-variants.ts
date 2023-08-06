de96087dda5be648fb0c90cd038a26cdadeaf1b6
Alessandro Bortolin <bortolin.alessandro@outlook.it>
2023-08-06 16:42:06 +0200

    Avoid assigning variant variables to global scope

--- src/mdx/recma-variants.ts
+++ src/mdx/recma-variants.ts
@@ -1,89 +1,73 @@
-import { ExpressionStatement, Program } from "estree";
-import { visit } from "estree-util-visit";
+import { Program } from "estree";
+import { builders as b, is, traverse } from "estree-toolkit";
 import { Plugin } from "unified";
 
 const recmaVariants: Plugin<[], Program> = () => {
-  const variant = parseInt(process.env.QUIZMS_VARIANT ?? "");
-
   return (ast) => {
-    let variantsFound = false;
-
-    visit(ast, (node) => {
-      if (
-        node.type === "VariableDeclarator" &&
-        node.id.type === "Identifier" &&
-        node.id.name === "variants"
-      ) {
-        if (node.init?.type !== "ArrayExpression") {
-          throw new Error(
-            `invalid problem variants: variable's type must be an array, \`${node.init?.type}\` is not an array`,
-          );
-        }
-        variantsFound = true;
-        if (variant >= 0 && variant < node.init.elements.length) {
-          node.init.elements = node.init.elements.slice(variant, variant + 1);
-        }
-      }
-      if (
-        node.type === "FunctionDeclaration" &&
-        node.id?.name === "_createMdxContent" &&
-        variantsFound
-      ) {
-        node.body.body.unshift(buildDeclaration());
-      }
-    });
+    const found = findVariants(ast);
+    if (found) {
+      injectLocalVariables(ast);
+    }
   };
 };
 
 export default recmaVariants;
 
-function buildDeclaration(): ExpressionStatement {
-  return {
-    type: "ExpressionStatement",
-    expression: {
-      type: "CallExpression",
-      callee: {
-        type: "MemberExpression",
-        object: {
-          type: "Identifier",
-          name: "Object",
-        },
-        property: {
-          type: "Identifier",
-          name: "assign",
-        },
-        computed: false,
-        optional: false,
-      },
-      arguments: [
-        {
-          type: "Identifier",
-          name: "globalThis",
-        },
-        {
-          type: "MemberExpression",
-          object: {
-            type: "Identifier",
-            name: "variants",
-          },
-          property: {
-            type: "MemberExpression",
-            object: {
-              type: "Identifier",
-              name: "props",
-            },
-            property: {
-              type: "Identifier",
-              name: "variant",
-            },
-            computed: false,
-            optional: false,
-          },
-          computed: true,
-          optional: false,
-        },
-      ],
-      optional: false,
+function findVariants(ast: Program) {
+  const variant = parseInt(process.env.QUIZMS_VARIANT ?? "");
+
+  let variantsFound = false;
+  traverse(ast, {
+    VariableDeclarator(path) {
+      const node = path.node!;
+      if (is.identifier(node.id) && node.id.name === "variants") {
+        const init = node.init;
+        if (!is.arrayExpression(init)) {
+          throw new Error(
+            `invalid problem variants: variable's type must be an array, \`${init?.type}\` is not an array`,
+          );
+        }
+        variantsFound = true;
+        if (variant >= 0 && variant < init.elements.length) {
+          // TODO
+          init.elements = init.elements.slice(variant, variant + 1);
+        }
+      }
     },
-  };
+  });
+
+  return variantsFound;
+}
+
+const wellKnownGlobals = new Set(["String", "Object", "Error", "Math"]);
+
+function injectLocalVariables(ast: Program) {
+  traverse(ast, {
+    $: { scope: true },
+
+    FunctionDeclaration(path) {
+      const node = path.node!;
+      if (node.id?.name === "_createMdxContent") {
+        node.body.body.unshift(
+          b.variableDeclaration("const", [
+            b.variableDeclarator(
+              b.identifier("_v"),
+              b.memberExpression(
+                b.identifier("variants"),
+                b.memberExpression(b.identifier("props"), b.identifier("variant")),
+                true,
+              ),
+            ),
+          ]),
+        );
+      }
+    },
+
+    Identifier(path) {
+      const name = path.node!.name;
+      if (path.scope!.hasGlobalBinding(name) && !wellKnownGlobals.has(name)) {
+        path.replaceWith(b.memberExpression(b.identifier("_v"), b.identifier(name))).skip();
+      }
+    },
+  });
 }
