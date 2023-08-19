c8ae4832ce8a6510faaa3e17eff6dd703e6b9bef
Alessandro Bortolin <bortolin.alessandro@outlook.it>
2023-08-19 14:46:58 +0200

    Improve images

--- src/mdx/remark-images.ts
+++ src/mdx/remark-images.ts
@@ -1,24 +1,29 @@
+import path from "node:path";
+
 import { Parser } from "acorn";
 import { Directive, Expression, ModuleDeclaration, Program } from "estree";
 import { builders as b } from "estree-toolkit";
 import _ from "lodash";
-import { Image, Parent, Root } from "mdast";
-import { MdxjsEsm } from "mdast-util-mdxjs-esm";
+import { Image, Parent, Root, Text } from "mdast";
+import { MdxJsxTextElement, MdxjsEsm } from "mdast-util-mdx";
 import { Plugin } from "unified";
-import { visit } from "unist-util-visit";
+import { Node } from "unist";
+import { SKIP, visit } from "unist-util-visit";
 
-import { jsxAttribute } from "~/mdx/utils";
+import { jsxAttribute } from "./utils";
 
 const remarkImages: Plugin<[], Root> = () => {
   return (tree: Root) => {
     const imports: ModuleDeclaration[] = [];
+
     visit(tree, "image", (image: Image, index, parent: Parent | undefined) => {
+      if (/^https?:\/\//.test(image.url)) return;
+      if (parent === undefined || index === undefined) {
+        throw new Error("Image must have a parent");
+      }
+
+      const url = (path.isAbsolute(image.url) ? "" : "./") + image.url;
       const { alt, title } = image;
-      let { url } = image;
-
-      if (/^https?:\/\//.test(url)) return;
-      if (!/^\.\.?\//.test(url)) url = `./${url}`;
-
       const name = `__img__${imports.length}__`;
 
       let imgSrc: Expression;
@@ -66,18 +71,47 @@ const remarkImages: Plugin<[], Root> = () => {
         imgSrc = b.identifier(name);
       }
 
-      parent!.children[index!] = {
+      let siblingIndex = index - 1;
+      let sibling: Node<any> | undefined = parent.children[siblingIndex];
+      while (sibling?.type === "text" && /^\s*$/.test((sibling as Text).value)) {
+        siblingIndex -= 1;
+        sibling = parent.children[siblingIndex];
+      }
+
+      let container: MdxJsxTextElement;
+      if (sibling?.data?.imgContainer) {
+        container = sibling as MdxJsxTextElement;
+        parent.children.splice(siblingIndex + 1, index - siblingIndex);
+        index = siblingIndex + 1;
+      } else {
+        container = {
+          type: "mdxJsxTextElement",
+          name: "span",
+          attributes: [jsxAttribute("className", "flex justify-center")],
+          data: { imgContainer: true },
+          children: [],
+        };
+        parent.children[index] = container;
+      }
+
+      container.children.push({
         type: "mdxJsxTextElement",
         name: "img",
         children: [],
         attributes: _.compact([
+          jsxAttribute(
+            "className",
+            "max-h-screen max-w-full p-4 first:rounded-l-xl last:rounded-r-xl dark:bg-white",
+          ),
           jsxAttribute("alt", alt),
           jsxAttribute("src", b.memberExpression(imgSrc, b.identifier("src"))),
           jsxAttribute("width", b.memberExpression(imgSrc, b.identifier("width"))),
           jsxAttribute("height", b.memberExpression(imgSrc, b.identifier("height"))),
           title && jsxAttribute("title", title),
         ]),
-      };
+      });
+
+      return [SKIP, index];
     });
 
     tree.children.unshift({
