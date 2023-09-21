9d10804c2a0c2c61390c7a0fe72452346ec57638
Alessandro Bortolin <bortolin.alessandro@outlook.it>
2023-09-21 22:39:13 +0200

    Fix open answers

--- src/mdx/remark-answers.ts
+++ src/mdx/remark-answers.ts
@@ -1,3 +1,5 @@
+import { Parser } from "acorn";
+import { Directive, Program } from "estree";
 import _ from "lodash";
 import { Blockquote, List, Paragraph, Parent, Root } from "mdast";
 import { MdxJsxFlowElement } from "mdast-util-mdx-jsx";
@@ -44,15 +46,32 @@ function parseMultipleAnswerGroup(tree: Root) {
 function parseOpenAnswerGroup(tree: Root) {
   visit(tree, "paragraph", (paragraph: Paragraph, index, parent) => {
     const text = paragraph.children[0];
-    if (text.type !== "text") return;
+    if (text?.type !== "text") return;
     if (!text.value.startsWith("?> ")) return;
 
-    const value = text.value.slice(3);
+    text.value = text.value.slice(3);
+
+    let templateLiteral = "String.raw`";
+    for (const child of paragraph.children) {
+      if (child.type === "text") {
+        templateLiteral += child.value;
+      } else if (child.type === "mdxTextExpression") {
+        templateLiteral += `$\{${child.value}}`;
+      } else {
+        throw new Error("Open answer solution must be in plain text");
+      }
+    }
+    templateLiteral += "`";
+
+    const template = Parser.parse(templateLiteral, {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    }) as unknown as Program;
 
     const attributes = _.compact([
-      jsxAttribute("type", /^\d+$/.test(value) ? "number" : "text"),
+      jsxAttribute("type", "text"),
       (process.env.NODE_ENV === "development" || process.env.QUIZMS_MODE === "training") &&
-        jsxAttribute("correct", value),
+        jsxAttribute("correct", (template.body[0] as Directive).expression),
     ]);
 
     parent!.children[index!] = {
