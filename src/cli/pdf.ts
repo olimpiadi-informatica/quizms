91dd49e7a3e4e42fe24c39e58583f8934167b8e3
Francesco Lugli <francilu26@gmail.com>
2023-12-07 18:04:17 +0100

    finalize pdf command to actually print pdfs

--- src/cli/pdf.ts
+++ src/cli/pdf.ts
@@ -2,6 +2,7 @@ import { join } from "node:path";
 import { pathToFileURL } from "node:url";
 
 import express from "express";
+import { BrowserContext, chromium } from "playwright";
 import { temporaryDirectory } from "tempy";
 import { InlineConfig, build, mergeConfig } from "vite";
 
@@ -9,14 +10,7 @@ import { parseContest } from "~/jsx-runtime/parser";
 
 import configs from "./vite/configs";
 
-export type PdfOptions = {
-  dir: string;
-  outDir: string;
-  variant?: string;
-  contest?: string;
-};
-
-export default async function pdf(options: PdfOptions) {
+async function pdfServer(dir: string, contest: string, port: number) {
   process.env.QUIZMS_MODE = "pdf";
 
   const defaultConfig = configs("production", {
@@ -26,18 +20,18 @@ export default async function pdf(options: PdfOptions) {
     },
   });
 
-  const outDir = join(options.dir, options.outDir);
+  const outDir = temporaryDirectory();
   const serverDir = temporaryDirectory();
   const fileName = "base-contest";
 
   const bundleConfig: InlineConfig = {
-    root: join(options.dir, "src"),
+    root: join(dir, "src"),
     build: {
       copyPublicDir: false,
       outDir,
       emptyOutDir: true,
       lib: {
-        entry: options.contest ?? "contest/contest.mdx",
+        entry: contest,
         fileName,
         formats: ["es"],
       },
@@ -45,7 +39,7 @@ export default async function pdf(options: PdfOptions) {
   };
 
   const serverConfig: InlineConfig = {
-    root: join(options.dir, "src"),
+    root: join(dir, "src"),
     build: {
       copyPublicDir: false,
       outDir: serverDir,
@@ -66,8 +60,8 @@ export default async function pdf(options: PdfOptions) {
   await build(mergeConfig(defaultConfig, serverConfig));
 
   const app = express();
-  app.listen(3001, () => {
-    console.log("Server listening on port 3001");
+  const server = app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
   });
 
   app.get("/variant.js", (req, res) => {
@@ -77,4 +71,59 @@ export default async function pdf(options: PdfOptions) {
   });
 
   app.use(express.static(serverDir));
+  return server;
+}
+
+async function printVariant(
+  context: BrowserContext,
+  variant_id: string,
+  outDir: string,
+  serverPort: number,
+) {
+  const page = await context.newPage();
+  const url = `localhost:${serverPort}/print.html?variant=${variant_id}`;
+  const path = `${outDir}/variant_${variant_id}.pdf`;
+  console.info(`Printing variant ${variant_id}`);
+  await page.goto(url, { waitUntil: "load" });
+  for (const img of await page.getByRole("img").all()) {
+    await img.isVisible();
+  }
+  await page.waitForTimeout(10 * 1000);
+  await page.pdf({
+    path,
+    format: "a4",
+  });
+  console.info(`Printed variant ${variant_id} to file ${path}`);
+  await page.close();
+}
+
+export async function printVariants(variant_ids: string[], outDir: string, serverPort: number) {
+  const browser = await chromium.launch({ headless: true });
+  const context = await browser.newContext();
+  const chunkSize = 10;
+  for (let i = 0; i < variant_ids.length; i += chunkSize) {
+    const tasks = [];
+    for (let variant_id of variant_ids.slice(i, i + chunkSize)) {
+      tasks.push(printVariant(context, variant_id.toString(), outDir, serverPort));
+    }
+    await Promise.all(tasks);
+  }
+  await context.close();
+  await browser.close();
+}
+
+export type PdfOptions = {
+  dir: string;
+  outDir: string;
+  port: number;
+  count: number;
+  contest: string;
+};
+
+export default async function pdf(options: PdfOptions) {
+  console.log(options);
+  const server = await pdfServer(options.dir, options.contest, options.port);
+  const variant_ids = [...Array(options.count).keys()].map((x) => x.toString());
+  await printVariants(variant_ids, options.outDir, options.port);
+  server.close();
 }
