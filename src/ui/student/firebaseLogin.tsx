c5ca241462a41182e8fb55b66124b35d86e658bf
davi-bart <davide30.bartoli@gmail.com>
2023-12-10 01:28:37 +0100

    minor fix

--- src/ui/student/firebaseLogin.tsx
+++ src/ui/student/firebaseLogin.tsx
@@ -192,7 +192,7 @@ function StudentLogin({ header }: { header: ComponentType<any> }) {
         <Modal ref={modalRef} title="Attenzione">
           <p>
             Il tuo account è già presente su un'altro dispositivo. Per trasferire l'accesso al
-            dispositivo corrente comunica al tuo insegnante il codice seguente:
+            dispositivo corrente comunica al tuo insegnante il seguente codice di conferma:
           </p>
           <div className="flex justify-center pt-3">
             <span className="pt-1 font-mono text-3xl">
