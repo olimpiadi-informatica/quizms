189d5e010520e677510ca924f22aca303a913ba8
Alessandro Bortolin <bortolin.alessandro@outlook.it>
2023-12-16 17:36:27 +0100

    Export command

--- src/cli/firebase/export.ts
+++ src/cli/firebase/export.ts
@@ -1,66 +1,60 @@
-import { readFile, writeFile } from "node:fs/promises";
+import { mkdir, open, readFile } from "node:fs/promises";
+import { format, join } from "node:path";
 
 import { cert, deleteApp, initializeApp } from "firebase-admin/app";
-import { getFirestore } from "firebase-admin/firestore";
-import { range } from "lodash-es";
-import PapaParse from "papaparse";
+import { CollectionReference, getFirestore } from "firebase-admin/firestore";
 
-import { studentConverter } from "~/firebase/convertersAdmin";
-import { Student } from "~/models/student";
+import { schoolConverter, studentConverter, submissionConverter } from "~/firebase/convertersAdmin";
 
-export default async function exportStudents() {
+type ExportOptions = {
+  schools?: boolean;
+  students?: boolean;
+  submissions?: boolean;
+};
+
+export default async function exportContests(options: ExportOptions) {
   const serviceAccount = JSON.parse(await readFile("serviceAccountKey.json", "utf-8"));
   const app = initializeApp({
     credential: cert(serviceAccount),
   });
   const db = getFirestore(app);
 
-  const snapshot = await db.collection("students").withConverter(studentConverter).get();
+  const timestamp = new Date().toISOString();
+  const dir = join("export", timestamp);
+  await mkdir(dir, { recursive: true });
 
-  const students = snapshot.docs.map((doc) => doc.data());
-
-  const data = students
-    .filter((student) => !student.disabled && student.school !== "TEST00000X" && !isEmpty(student))
-    .map((student) => [
-      student.school,
-      student.contest,
-      student.personalInformation?.classYear,
-      (student.personalInformation?.classSection as string)?.trim(),
-      (student.personalInformation?.surname as string)?.trim(),
-      (student.personalInformation?.name as string)?.trim(),
-      (student.personalInformation?.birthDate as Date)?.toLocaleDateString(),
-      student.variant,
-      ...range(16).map((i) => student.answers?.[i]),
-      student.createdAt?.toLocaleString(),
-      student.updatedAt?.toLocaleString(),
-      student.id,
-    ]);
-  const schools = new Set(students.map((student) => student.school));
-
-  console.info(`${data.length} students found from ${schools.size} different schools.`);
-
-  const fields = [
-    "school",
-    "contest",
-    "classYear",
-    "classSection",
-    "surname",
-    "name",
-    "birthDate",
-    "variant",
-    ...range(16).map((i) => `q${i + 1}`),
-    "createdAt",
-    "updatedAt",
-    "id",
-  ];
-
-  data.sort();
-
-  await writeFile("students.csv", PapaParse.unparse({ fields, data }));
+  if (options.students) {
+    const ref = db.collection("students").withConverter(studentConverter);
+    await exportCollection(ref, "students", dir);
+  }
+  if (options.schools) {
+    const ref = db.collection("schools").withConverter(schoolConverter);
+    await exportCollection(ref, "schools", dir);
+  }
+  if (options.submissions) {
+    const ref = db.collection("submissions").withConverter(submissionConverter);
+    await exportCollection(ref, "submissions", dir);
+  }
 
   await deleteApp(app);
 }
 
-function isEmpty(student: Student) {
-  return Object.values(student.personalInformation ?? {}).every((field) => !field);
+async function exportCollection(ref: CollectionReference, collection: string, dir: string) {
+  const chunkSize = 25_000;
+  let snapshot = await ref.limit(chunkSize).get();
+
+  const file = await open(format({ dir, name: collection, ext: ".jsonl" }), "w");
+  let sum = 0;
+
+  while (!snapshot.empty) {
+    for (const doc of snapshot.docs) {
+      await file.write(JSON.stringify(doc.data()) + "\n");
+    }
+
+    sum += snapshot.size;
+    console.log(`Found ${sum} ${collection}.`);
+
+    const last = snapshot.docs.at(-1);
+    snapshot = await ref.startAfter(last).limit(chunkSize).get();
+  }
 }
--- src/cli/firebase/index.ts
+++ src/cli/firebase/index.ts
@@ -1,11 +1,20 @@
 import { Command } from "commander";
 
+import exportContests from "./export";
 import importContests from "./import";
 
 export default function firebaseCommand() {
   const command = new Command("firebase");
 
   command.description("commands to interact with the Firebase database.");
+
+  command
+    .command("export")
+    .description("export the contests.")
+    .option("--schools", "Export the schools.")
+    .option("--students", "Export the students.")
+    .option("--submissions", "Export the submissions.")
+    .action((options) => void exportContests(options));
 
   command
     .command("import")
@@ -18,7 +27,7 @@ export default function firebaseCommand() {
     .option("-z, --solutions", "Import the solutions.")
     .option("-m, --mappings", "Import the variant mappings.")
     .option("-p, --pdfs", "Import the pdf files.")
-    .option("-d, --delete", "Delete existings collections")
+    .option("-d, --delete", "Delete existing collections")
     .option("-a, --all", "Import everything.")
     .action((options) => void importContests(options));
 
