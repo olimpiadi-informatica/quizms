e3b4420cfdedbf5a9badbf5719b71d1c3de2c44c
Alessandro Bortolin <bortolin.alessandro@outlook.it>
2024-02-14 14:45:50 +0100

    Another minor fix

--- src/mdx/components/image.tsx
+++ src/mdx/components/image.tsx
@@ -15,13 +15,13 @@ type Props = {
 export default function Image({ src, alt, title }: Props) {
   return (
     <img
-      className="max-h-screen min-w-0 p-4 first:rounded-l-xl last:rounded-r-xl dark:bg-white print:m-0 print:max-h-[60vh] print:max-w-full"
+      className="max-h-[90vh] min-w-0 p-4 first:rounded-l-xl last:rounded-r-xl dark:bg-white print:m-0 print:max-h-[60vh] print:max-w-full"
       src={src.src}
       width={src.width}
       height={src.height}
       alt={alt}
       title={title}
-      style={{ maxWidth: `min(100%, calc(100vh * ${src.width} / ${src.height}))` }}
+      style={{ maxWidth: `min(100%, calc(90vh * ${src.width} / ${src.height}))` }}
     />
   );
 }
