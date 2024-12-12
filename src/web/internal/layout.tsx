871c32162be4b1d9ed26c1329cc4e6312fd5daca
Alessandro Bortolin <bortolin.alessandro@outlook.it>
2024-12-12 23:35:57 +0100

    Use more precise dates

--- src/web/internal/layout.tsx
+++ src/web/internal/layout.tsx
@@ -1,7 +1,7 @@
 import { type ReactNode, Suspense, useDeferredValue } from "react";
 
 import { Layout, useNotifications } from "@olinfo/react-components";
-import { SWRConfig } from "swr";
+import useSWR, { SWRConfig } from "swr";
 import { type BaseLocationHook, Router, Switch, useLocation } from "wouter";
 
 import { Loading } from "~/web/components";
@@ -21,6 +21,7 @@ export function BaseLayout({ children }: { children: ReactNode }) {
           <Router hook={hook}>
             <Switch>{children}</Switch>
           </Router>
+          <DatePolyfill />
         </Suspense>
       </LayoutInner>
     </Layout>
@@ -31,4 +32,41 @@ function LayoutInner({ children }: { children: ReactNode }) {
   const { notifyError } = useNotifications();
 
   return <SWRConfig value={{ onError: notifyError }}>{children}</SWRConfig>;
+}
+
+function DatePolyfill() {
+  globalThis.NativeDate ??= Date;
+
+  const { data: timeDelta } = useSWR(
+    "https://time1.olinfo.it",
+    async (url) => {
+      const res = await fetch(url);
+      const now = globalThis.NativeDate.now();
+      const timestamp = await res.text();
+      return Number(timestamp) - now;
+    },
+    {
+      revalidateIfStale: false,
+      revalidateOnMount: true,
+      suspense: true,
+    },
+  );
+
+  class PreciseDate extends globalThis.NativeDate {
+    constructor(...args: any[]) {
+      super(...((args.length === 0 ? [PreciseDate.now()] : args) as []));
+    }
+
+    static now() {
+      return globalThis.NativeDate.now() + timeDelta;
+    }
+  }
+
+  globalThis.Date = PreciseDate as any;
+
+  return null;
+}
+
+declare global {
+  var NativeDate: typeof Date;
 }
