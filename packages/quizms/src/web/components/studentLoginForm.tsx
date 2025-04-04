a6345a53b9c8bdd9472f1dd967d15bb61209a252
Francesco Lugli <francilu26@gmail.com>
2025-04-04 12:13:26 +0200

    create StudentLoginForm component

---
+++ packages/quizms/src/web/components/studentLoginForm.tsx
@@ -0,0 +1,72 @@
+import { useRef } from "react";
+
+import {
+  Form,
+  Navbar,
+  NavbarBrand,
+  SelectField,
+  SubmitButton,
+  TextField,
+} from "@olinfo/react-components";
+import { mapValues } from "lodash-es";
+
+import { UserDataField } from "~/web/student/user-data-form";
+import { Contest, Student } from "~/models";
+import { useMetadata } from "./page";
+
+export type FormStudent = {
+  contestId: string;
+  token: string;
+} & Student["userData"];
+
+export function StudentLoginForm({
+  contests,
+  onSubmit,
+}: {
+  contests: Contest[];
+  onSubmit: ({ contestId, token, ...userData }: FormStudent) => Promise<void>;
+}) {
+  const defaultValue = {
+    contestId:
+      Object.keys(contests).length === 1 ? Object.keys(contests)[0] : undefined,
+  };
+  const contestsMap = Object.fromEntries(
+    contests.map((contest) => [contest.id, contest]),
+  );
+  const metadata = useMetadata();
+  <>
+    <Navbar color="bg-base-300 text-base-content">
+      <NavbarBrand>
+        <div className="flex items-center h-full font-bold">
+          {metadata.title}
+        </div>
+      </NavbarBrand>
+    </Navbar>
+    <Form defaultValue={defaultValue} onSubmit={onSubmit} className="p-4 pb-8">
+      <h1 className="mb-2 text-xl font-bold">Accedi alla gara</h1>
+      <SelectField
+        field="contestId"
+        label="Gara"
+        options={mapValues(contests, "name")}
+        placeholder="Seleziona una gara"
+      />
+      {({ contestId }) => {
+        const contest = contestsMap[contestId ?? ""];
+        if (!contest) return;
+        return (
+          <>
+            {contest.userData.map((field) => (
+              <UserDataField key={field.name} field={field} />
+            ))}
+            <TextField
+              field="token"
+              label="Codice prova"
+              placeholder="aaaaa-bbbbb-ccccc"
+            />
+            <SubmitButton>Inizia la prova</SubmitButton>
+          </>
+        );
+      }}
+    </Form>
+  </>;
+}
--- packages/quizms/src/web/firebase/student-login.tsx
+++ packages/quizms/src/web/firebase/student-login.tsx
@@ -28,8 +28,14 @@ import {
   studentMappingUidConverter,
   submissionConverter,
 } from "./common/converters";
-import { useAnonymousAuth, useCollection, useDocument, useDocumentOptional } from "./hooks";
+import {
+  useAnonymousAuth,
+  useCollection,
+  useDocument,
+  useDocumentOptional,
+} from "./hooks";
 import { DuplicateStudentError, loginAction } from "./student-login-action";
+import StudentLoginForm, { FormStudent } from "../components/studentLoginForm";
 
 type LoginProps = {
   config: FirebaseOptions;
@@ -37,18 +43,26 @@ type LoginProps = {
   children: ReactNode;
 };
 
-export function FirebaseStudentLogin({ config, contestFilter, children }: LoginProps) {
+export function FirebaseStudentLogin({
+  config,
+  contestFilter,
+  children,
+}: LoginProps) {
   return (
     <FirebaseLogin config={config}>
-      <StudentLoginInner contestFilter={contestFilter}>{children}</StudentLoginInner>
+      <StudentLoginInner contestFilter={contestFilter}>
+        {children}
+      </StudentLoginInner>
     </FirebaseLogin>
   );
 }
 
-function StudentLoginInner({ contestFilter, children }: Omit<LoginProps, "config">) {
+function StudentLoginInner({
+  contestFilter,
+  children,
+}: Omit<LoginProps, "config">) {
   const db = useDb();
   const user = useAnonymousAuth();
-  const metadata = useMetadata();
 
   const [contests] = useCollection("contests", contestConverter, {
     subscribe: true,
@@ -60,10 +74,8 @@ function StudentLoginInner({ contestFilter, children }: Omit<LoginProps, "config
     { subscribe: true },
   );
 
-  const filteredContests = Object.fromEntries(
-    contests
-      .filter((contest) => contestFilter?.includes(contest.id) ?? true)
-      .map((contest) => [contest.id, contest]),
+  const filteredContests = contests.filter(
+    (contest) => contestFilter?.includes(contest.id) ?? true,
   );
 
   const modalRef = useRef<HTMLDialogElement>(null);
@@ -73,21 +85,12 @@ function StudentLoginInner({ contestFilter, children }: Omit<LoginProps, "config
       <StudentInner
         contests={contests}
         participationId={studentMapping.participationId}
-        studentId={studentMapping.studentId}>
+        studentId={studentMapping.studentId}
+      >
         {children}
       </StudentInner>
     );
   }
-
-  const defaultValue = {
-    contestId:
-      Object.keys(filteredContests).length === 1 ? Object.keys(filteredContests)[0] : undefined,
-  };
-
-  type FormStudent = {
-    contestId: string;
-    token: string;
-  } & Student["userData"];
 
   const submit = async ({ contestId, token, ...userData }: FormStudent) => {
     try {
@@ -108,37 +111,12 @@ function StudentLoginInner({ contestFilter, children }: Omit<LoginProps, "config
 
   return (
     <>
-      <Navbar color="bg-base-300 text-base-content">
-        <NavbarBrand>
-          <div className="flex items-center h-full font-bold">{metadata.title}</div>
-        </NavbarBrand>
-      </Navbar>
-      <Form defaultValue={defaultValue} onSubmit={submit} className="p-4 pb-8">
-        <h1 className="mb-2 text-xl font-bold">Accedi alla gara</h1>
-        <SelectField
-          field="contestId"
-          label="Gara"
-          options={mapValues(filteredContests, "name")}
-          placeholder="Seleziona una gara"
-        />
-        {({ contestId }) => {
-          const contest = filteredContests[contestId ?? ""];
-          if (!contest) return;
-          return (
-            <>
-              {contest.userData.map((field) => (
-                <UserDataField key={field.name} field={field} />
-              ))}
-              <TextField field="token" label="Codice prova" placeholder="aaaaa-bbbbb-ccccc" />
-              <SubmitButton>Inizia la prova</SubmitButton>
-            </>
-          );
-        }}
-      </Form>
+      <StudentLoginForm contests={filteredContests} onSubmit={submit}/>
       <Modal ref={modalRef} title="Attenzione">
         <p>
-          Il tuo account è già presente su un&apos;altro dispositivo. Per trasferire l&apos;accesso
-          al dispositivo corrente comunica al tuo insegnante il codice seguente:
+          Il tuo account è già presente su un&apos;altro dispositivo. Per
+          trasferire l&apos;accesso al dispositivo corrente comunica al tuo
+          insegnante il codice seguente:
         </p>
         <div className="flex justify-center pt-3">
           <span className="pt-1 font-mono text-3xl">
@@ -192,7 +170,9 @@ function StudentInner({
 
     await setStudent({ ...newStudent });
 
-    const submissionRef = collection(db, "submissions").withConverter(submissionConverter);
+    const submissionRef = collection(db, "submissions").withConverter(
+      submissionConverter,
+    );
     await addDoc(submissionRef, {
       id: "",
       uid: newStudent.uid!,
@@ -208,7 +188,8 @@ function StudentInner({
       setStudent={setStudentAndSubmission}
       logout={logout}
       reset={logout}
-      onSubmit={onSubmit}>
+      onSubmit={onSubmit}
+    >
       {children}
     </StudentProvider>
   );
