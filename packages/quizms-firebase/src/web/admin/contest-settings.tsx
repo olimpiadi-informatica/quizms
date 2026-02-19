55e893068ce6c90059eba59995789f8cbc696150
Francesco Lugli <francilu26@gmail.com>
2026-02-19 15:54:53 +0100

    Allow configuration of scoreVisibility using the admin interface

--- packages/quizms-firebase/src/web/admin/contest-settings.tsx
+++ packages/quizms-firebase/src/web/admin/contest-settings.tsx
@@ -1,18 +1,23 @@
 import type { Contest } from "@olinfo/quizms/models";
-import { Form, SubmitButton, TextAreaField } from "@olinfo/react-components";
+import { Form, SelectField, SubmitButton, TextAreaField } from "@olinfo/react-components";
 
 import { useAdmin } from "./context";
 
 export default function ContestSettings() {
   const { contest, setContest } = useAdmin();
 
-  const save = async (newContest: Pick<Contest, "instructions">) => {
+  const save = async (newContest: Pick<Contest, "instructions" | "scoreVisibility">) => {
     await setContest({ ...contest, ...newContest });
   };
 
   return (
     <Form key={contest.id} defaultValue={contest} onSubmit={save} className="!max-w-full">
-      <TextAreaField field="instructions" label="Istruzioni gara" rows={8} />
+      <SelectField
+        field="scoreVisibility"
+        label="Mostra punteggi"
+        options={{ never: "Mai", always: "Sempre", finalized: "Solo alle scuole finalizzate" }}
+      />
+      <TextAreaField field="instructions" label="Istruzioni gara" rows={8} optional={true} />
       <SubmitButton>Salva</SubmitButton>
     </Form>
   );
