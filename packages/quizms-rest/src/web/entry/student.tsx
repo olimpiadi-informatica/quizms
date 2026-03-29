c89b74719e26bc37fc33461a7b299291b33a513f
Francesco Lugli <francilu26@gmail.com>
2026-03-29 19:12:55 +0200

    Remove unused import

--- packages/quizms-rest/src/web/entry/student.tsx
+++ packages/quizms-rest/src/web/entry/student.tsx
@@ -1,4 +1,4 @@
-import { startTransition, useCallback } from "react";
+import { useCallback } from "react";
 
 import { Title } from "@olinfo/quizms/components";
 import type { Answer } from "@olinfo/quizms/models";
