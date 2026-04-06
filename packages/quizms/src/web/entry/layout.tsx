d2915c4d729e69a298cad5034e483c9d661e38bf
Francesco Lugli <francilu26@gmail.com>
2026-04-06 21:58:03 +0200

    fix time api in subpath

--- packages/quizms/src/web/entry/layout.tsx
+++ packages/quizms/src/web/entry/layout.tsx
@@ -41,7 +41,7 @@ function DatePolyfill() {
   globalThis.NativeDate ??= Date;
 
   const { data: timeDelta } = useSWR(
-    "/api/time",
+    `${process.env.BASE_PATH}api/time`,
     async (url) => {
       const res = await fetch(url);
       const now = globalThis.NativeDate.now();
