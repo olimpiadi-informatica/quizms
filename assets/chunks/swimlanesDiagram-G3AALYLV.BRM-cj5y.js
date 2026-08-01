5097873f33a32ee58478b8626e005ece5eac471c
bortoz <bortoz@users.noreply.github.com>
2026-08-01 14:26:22 +0000

    deploy: ddc8e7b776fe26dd253ce5d73b68eea842717357

---
+++ assets/chunks/swimlanesDiagram-G3AALYLV.BRM-cj5y.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U.BXj0EfZx.js";import{_ as a}from"../app.CaWXkzbS.js";import"./chunk-5VM5RSS4.DsrX1LBE.js";import"./chunk-XXDRQBXY.Cg7g1k0y.js";import"./chunk-VR4S4FIN.C-1y3aZq.js";import"./chunk-32BRIVSS.VI6Lf8Uh.js";import"./channel.D1w4fT5s.js";import"./framework.B1UNJuMu.js";import"./theme.BvAQ1xXL.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
