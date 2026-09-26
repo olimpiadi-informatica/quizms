ebb9f8269b44a4fab37663c40b2fae730ae8e5e1
Alenygam <alessandro@farcas.it>
2026-09-26 17:35:45 +0200

    Fixed typos not allowing contests to import or start properly

---
+++ .changeset/pretty-ties-live.md
@@ -0,0 +1,5 @@
+---
+"@olinfo/quizms-firebase": patch
+---
+
+Fixed some typos creating invalid schemas for users in the DB. Added the correct file extensions for copying the statements to Firebase Storage.
--- packages/quizms-firebase/src/cli/import.ts
+++ packages/quizms-firebase/src/cli/import.ts
@@ -213,7 +213,7 @@ async function importStatements(bucket: Bucket, options: ImportOptions) {
       async (id): Promise<[string, string][]> => {
         const localDir = path.join("variants", config.id, id);
         const remoteDir = path.join("statements", config.id, id);
-        const files = await glob("*.{pdf,js,css}", { cwd: localDir });
+        const files = await glob("*.{pdf,css,json,txt,mjs}", { cwd: localDir });
 
         return files.flatMap((file) => {
           const ext = path.extname(file);
